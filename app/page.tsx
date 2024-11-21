6cf3d63c3fb492d5d9e6bde73bb3cd9d4acf345f
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-11-21 15:54:36 +0530

    update: nextjs 15.0.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
+'use client'
+
 import { ModeToggle } from "@/components/ModeToggle";
 
 const page = () => {
   return (
-    <div>
-      <h3>hello world</h3>
+    <div className="flex flex-col items-center justify-center h-screen container">
+      <div>
+        <p className="text-5xl font-light mb-10">Rajvansh Nextjs Starter Kit</p>
+      </div>
+      <div>
       <ModeToggle />
+      </div>
+      <div className="text-2xl">
+        <li>Next.js 15</li>
+        <li>sdf</li>
+        <li>sdf</li>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default page;
